cd228cfd8d934f6d2f30c39dae59a2fe2a4b6e9b
Nicolas <nisoffi@gmail.com>
2021-01-09 16:05:09 -0300

    visual feito

--- Store-Blogs-UI/src/app/app.module.ts
+++ Store-Blogs-UI/src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { HomeComponent } from './components/home/home.component';
 import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PostComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
